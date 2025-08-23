953ef42e9fe8f311cb1b4b3837f6987d7f536cb2
Sudipta Nath <nathsudipta7@gmail.com>
2025-08-23 15:19:48 +0530

    update build.bat adding broppy and egatho

--- build.bat
+++ build.bat
@@ -3,14 +3,22 @@ REM Batch file to build all games for Windows
 
 if not exist bin mkdir bin
 
-echo Building Serpente (Snake)...
-g++ -Wall -std=c++17 games\snake\serpente.cpp -o bin\snake.exe
-
 echo Building Traffigo (Car Race)...
 g++ -Wall -std=c++17 games\car_race\traffigo.cpp -o bin\car_race.exe
-
 echo Building Egatho (Egg Basket)...
 g++ -Wall -std=c++17 games\egg_basket\*.cpp -o bin\egg_basket.exe
+
+echo Building Serpente (Snake)...
+g++ -Wall -std=c++17 games\serpente\serpente.cpp -o bin\serpente.exe
+
+echo Building Traffigo (Car Race)...
+g++ -Wall -std=c++17 games\traffigo\traffigo.cpp -o bin\traffigo.exe
+
+echo Building Egatho (Egg Basket)...
+g++ -Wall -std=c++17 games\egatho\egatho.cpp -o bin\egatho.exe
+
+echo Building Broppy (Breakout Clone)...
+g++ -Wall -std=c++17 games\broppy\broppy.cpp -o bin\broppy.exe
 
 echo Build complete.
 pause
